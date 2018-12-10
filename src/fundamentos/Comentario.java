68dd86cdf541c8e1a15b2d688185ca42c606455f
robsonsilv4 <robsonsilv410@gmail.com>
2018-12-10 10:13:40 -0300

    Comentários

---
+++ src/fundamentos/Comentario.java
@@ -0,0 +1,25 @@
+package fundamentos;
+
+/**
+ * 
+ * @author robson [robsonsilv410@gmail.com]
+ * @version 1.0
+ * @since 0.1
+ *
+ */
+public class Comentario {
+
+	/**
+	 * Documentação
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/*
+		 * Múltiplas linhas
+		 */
+		System.out.println("Primeiro");
+
+		// Comentário óbvio
+		System.out.println("Programa!");
+	}
+}
